ebcd7ad86bc0fde71a196ba66b38a33ecf340941
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-04-04 14:39:11 +0100

    ✨ Feat(event list): create the event listing

--- src/main/java/com/events/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/events/passin/repositories/AttendeeRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.events.passin.domain.attendee.Attendee;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
-    
+    List<Attendee> findByEventId(String eventId);
 }
